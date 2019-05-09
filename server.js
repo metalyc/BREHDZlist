0a90886108c0de8603075330a2d531d06a0af000
Evan Aprhys <evan.aprhys@gmail.com>
2019-05-08 18:31:33 -0700

    added login and logout, code cleaning in server.js

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ const hbs = require('hbs');
 const firebase = require('firebase');
 //const admin = require("firebase-admin");
 const bodyParser = require('body-parser');
-const url = require('url');
-const fs = require('fs');
+//const url = require('url');
+//const fs = require('fs');
 const expressValidator = require('express-validator');
 
 var app = express();
-
-
-
-// app.use(session({ secret: 'krunal', resave: false, saveUninitialized: true }));
-// app.use(expressValidator());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
+/*
+app.use(session({ secret: 'krunal', resave: false, saveUninitialized: true }));
+app.use(expressValidator());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+*/
 var port = process.env.PORT || 8080;
 
 //Needed to use partials folder
@@ -96,28 +94,18 @@ app.get('/product/*', (req, res) => {
   res.render('productspageContinued.hbs');
 });
 
-//404 page
-app.get('*', (req, res) => {
-    res.status(404);
-    res.render('404.hbs', {
-        title: '404',
-        error: 'Page does not exist.'
-    });
-});
-
-
 //const firebase=require('firebase');
 const a = require('firebase/storage');
-  var config = {
-      apiKey: "AIzaSyD-JUCw1YT0kN7rFez1AZckOvLC3E5kcY0",
-      authDomain: "bhredz.firebaseapp.com",
-      databaseURL: "https://bhredz.firebaseio.com",
-      projectId: "bhredz",
-      storageBucket: "bhredz.appspot.com",
-      messagingSenderId: "37106834429"
-  };
-  firebase.initializeApp(config);
-  var db = firebase.firestore();
+var config = {
+    apiKey: "AIzaSyD-JUCw1YT0kN7rFez1AZckOvLC3E5kcY0",
+    authDomain: "bhredz.firebaseapp.com",
+    databaseURL: "https://bhredz.firebaseio.com",
+    projectId: "bhredz",
+    storageBucket: "bhredz.appspot.com",
+    messagingSenderId: "37106834429"
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
 //  console.log(db);
 
 
@@ -125,14 +113,11 @@ function addData(name, price, condition, location, image)
 {
   console.log(image);
 
-
-
     var thename = name;
     var theprice = price;
     var thecondition = condition;
     var thelocation = location;
     var theImg = image;
-
 
     function getImageForPath(p){
 //      console.log(p);
@@ -183,29 +168,7 @@ app.post('/firebase', function(request, response)
     response.redirect('/');
 });
 
-/* what even are these?
-app.get('/login', (req, res) => {
-    res.render('login.hbs', {
-        title: 'Login',
-        email: 'Email',
-        pass: 'Password'
-    });
-});
-
-// Signup page
-app.get('/signup', (req, res) => {
-    res.render('signup.hbs', {
-        title: 'Signup'
-    });
-*/
-//start server
-app.use(express.static(__dirname));
-var server = app.listen(process.env.PORT || 8080, () => {
-    console.log('server is listening on port', server.address().port);
-});
-
 // POST for user signup
-
 app.post('/newUser', (request, response) => {
     var email = request.body.email;
     var pwd1 = request.body.password1;
@@ -222,16 +185,54 @@ app.post('/newUser', (request, response) => {
     }
 });
 
-// post for login
+//post for login
 app.post('/actionlogin', (req, res) => {
   var email = req.body.email;
   var pass = req.body.pass;
   firebase.auth().signInWithEmailAndPassword(email, pass)
   .then(function() {
     console.log("logged in with", email);
+    app.locals.user = true;
+    res.redirect('/');
   })
   .catch(function(error){
     console.log("Error with code:", error.code, "\nWith message:", error.message);
   });
-  res.redirect('/');
 });
+
+//logout function
+app.get('/logout', (req, res) => {
+  firebase.auth().signOut()
+  .then(function() {
+    console.log("Signed out.");
+    app.locals.user = false;
+    res.redirect('/');
+  })
+  .catch(function(error) {
+    console.log("Error with code:", error.code, "\nWith message:", error.message);
+  });
+});
+
+
+/////////////////////////////
+//Place all code above here//
+/////////////////////////////
+
+//404 page
+app.get('*', (req, res) => {
+    res.status(404);
+    res.render('404.hbs', {
+        title: '404',
+        error: 'Page does not exist.'
+    });
+});
+
+//start server
+app.use(express.static(__dirname));
+var server = app.listen(process.env.PORT || 8080, () => {
+    console.log('server is listening on port', server.address().port);
+});
+
+/////////////////////////////////////////
+//Don't place code down here, scroll up//
+/////////////////////////////////////////
