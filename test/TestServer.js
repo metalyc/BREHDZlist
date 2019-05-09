d70da94bac0a561de147a25884c211c67d602f66
metalyc <evan.aprhys@gmail.com>
2019-05-08 21:06:55 -0700

    Fixed bulk commenting in TestServer.js

--- test/TestServer.js
+++ test/TestServer.js
@@ -13,44 +13,44 @@ const test = require('firebase-functions-test')({
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-//
-// chai.use(chaiHttp);
-//
-// // const app = require('../server.js');
-//
-// describe('GET /', function () {
-//     it("should return webpage with title of 'Welcome to the login page.' ", function (done) {
-//         chai.request('http://localhost:8080')
-//             .get('/')
-//             .end(function(err, res) {
-//                 expect('Content-Type', "text/html; charset=utf-8");
-//                 expect(res).to.have.status(200);
-//                 done()
-//             })
-//     });
-// });
-//
-// describe('GET /login', function () {
-//     it("should return the login page ", function (done) {
-//         chai.request('http://localhost:8080')
-//             .get('/login')
-//             .end(function(err, res) {
-//                 expect('Content-Type', "text/html; charset=utf-8");
-//                 expect(res).to.have.status(200);
-//                 done()
-//             })
-//     });
-// });
-//
-// describe('GET /apple', function () {
-//     it("Should not work, apple page does not exist ", function (done) {
-//         chai.request('http://localhost:8080')
-//             .get('/apple')
-//             .end(function(err, res) {
-//                 expect('Content-Type', "text/html; charset=utf-8");
-//                 expect(res).to.have.status(404);
-//                 done()
-//             })
-//     });
-// });
+/*
+chai.use(chaiHttp);
 
+const app = require('../server.js');
+
+describe('GET /', function () {
+    it("should return webpage with title of 'Welcome to the login page.' ", function (done) {
+        chai.request('http://localhost:8080')
+            .get('/')
+            .end(function(err, res) {
+                expect('Content-Type', "text/html; charset=utf-8");
+                expect(res).to.have.status(200);
+                done()
+            })
+    });
+});
+
+describe('GET /login', function () {
+    it("should return the login page ", function (done) {
+        chai.request('http://localhost:8080')
+            .get('/login')
+            .end(function(err, res) {
+                expect('Content-Type', "text/html; charset=utf-8");
+                expect(res).to.have.status(200);
+                done()
+            })
+    });
+});
+
+describe('GET /apple', function () {
+    it("Should not work, apple page does not exist ", function (done) {
+        chai.request('http://localhost:8080')
+            .get('/apple')
+            .end(function(err, res) {
+                expect('Content-Type', "text/html; charset=utf-8");
+                expect(res).to.have.status(404);
+                done()
+            })
+    });
+});
+*/
