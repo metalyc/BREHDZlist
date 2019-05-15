dbcdcd8d0e96d9f9bd4824132c18aa275296386f
metalyc <evan.aprhys@gmail.com>
2019-05-15 15:39:17 -0700

    Fine tuned image sizing in /products

--- views/Script.js
+++ views/Script.js
@@ -2,7 +2,7 @@ function getProducts(){
     firebase.firestore().collection("Products").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             document.getElementById('products').innerHTML += `<a class="card btn btn-outline-secondary mx-1 mt-2" href="products/` + doc._key.path.segments[6] + `" style="width: 16rem; height: 25rem">
-                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="height: auto; width: auto; max-width: 15rem; max-height: 15rem; margin-top: 5px; align: center;">
+                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="width: 256px; margin-left: -14px; margin-top: -8px;">
                     <div class="card-body">
                       <h5 class="card-title">` + doc.data().Name + `</h5>
                       <p class="card-text">Location: <b>` + doc.data().Location + `</b></p>
