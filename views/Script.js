6f5ab595b2940086e3bf8e66b9f42a2e139b3e62
metalyc <evan.aprhys@gmail.com>
2019-05-14 18:43:02 -0700

    Fixed image scaling on products page

--- views/Script.js
+++ views/Script.js
@@ -2,7 +2,7 @@ function getProducts(){
     firebase.firestore().collection("Products").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             document.getElementById('products').innerHTML += `<div class="card m-1" style="width: 16rem; height: 25rem">
-                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="height: 150px; margin-top: 5px">
+                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="height: auto; width: auto; max-width: 150px; max-height: 150px; margin-top: 5px; margin-left: auto; margin-right: auto;">
                     <div class="card-body">
                       <h5 class="card-title">` + doc.data().Name + `</h5>
                       <p class="card-text">Location: <b>` + doc.data().Location + `</b></p>
