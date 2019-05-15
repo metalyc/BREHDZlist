d44f996a2444142c7a8f4e5210bd339764e8f35e
metalyc <evan.aprhys@gmail.com>
2019-05-14 17:19:51 -0700

    Basic product details page working

--- views/Script.js
+++ views/Script.js
@@ -1,17 +1,16 @@
 function getProducts(){
     firebase.firestore().collection("Products").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            document.getElementById('products').innerHTML += `<div class="card m-1" style="width: 16rem; height: 23rem">
+            document.getElementById('products').innerHTML += `<div class="card m-1" style="width: 16rem; height: 25rem">
                     <img class="card-img-top" src="` + doc.data().Img + `" alt="Card image cap" style="max-height: 150px; margin-top: 5px">
                     <div class="card-body">
                       <h5 class="card-title">` + doc.data().Name + `</h5>
                       <p class="card-text">` + 'Location: <strong>' + doc.data().Location + '</strong>' + `</p>
                       <p class="card-text">` + 'Condition: <strong>' + doc.data().Condition + '</strong>' + `</p>
-
+                      <a class="btn btn-primary" href="products/` + doc._key.path.segments[6] + `">Details</a>
                     </div>
-                      
                       <div class="card-footer text-muted text-center">
-                      <p class="card-text"> $` + doc.data().Price + `</p>
+                        <p class="card-text"> $` + doc.data().Price + `</p>
                       </div>
                   </div>`
         });
