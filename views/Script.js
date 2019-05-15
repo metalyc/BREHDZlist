b51acf6a35bba12baabc014b223037528af16542
metalyc <evan.aprhys@gmail.com>
2019-05-15 16:09:32 -0700

    code cleaning for script.js and home.hbs

--- views/Script.js
+++ views/Script.js
@@ -2,13 +2,13 @@ function getProducts(){
     firebase.firestore().collection("Products").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             document.getElementById('products').innerHTML += `<a class="card btn btn-outline-secondary mx-1 mt-2" href="products/` + doc._key.path.segments[6] + `" style="width: 16rem; height: 25rem">
-                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="width: 256px; margin-left: -14px; margin-top: -8px;">
+                    <img class="card-img-top" src="` + doc.data().Img + `" alt="A picture of ` + doc.data().Name + `" style="max-height: 12rem; width: 16rem; margin-left: -14px; margin-top: -8px;">
                     <div class="card-body">
                       <h5 class="card-title">` + doc.data().Name + `</h5>
                       <p class="card-text">Location: <b>` + doc.data().Location + `</b></p>
                       <p class="card-text">Condition: <b>` + doc.data().Condition + `</b></p>
                     </div>
-                    <div class="card-footer text-center">
+                    <div class="card-footer">
                       <p class="card-text"> $` + doc.data().Price + `</p>
                     </div>
                   </a>`
