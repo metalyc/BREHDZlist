c21752a1866aeb3db3b5c7560a5881bf796b8b17
metalyc <evan.aprhys@gmail.com>
2019-05-15 19:12:48 -0700

    fixed hardcoded phone decrypt, syntax, indentation for server.js

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ hbs.registerHelper('siteName', () => {
     return 'BREHDZlist';
 });
 
+hbs.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 ////////////////
 //Page routing//
 ////////////////
@@ -93,13 +100,6 @@ app.get('/signup', (req, res) => {
 //product detail pages
 //search page
 
-hbs.registerHelper('ifCond', function(v1, v2, options) {
-  if(v1 === v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
 app.get('/search', (req, res) => {
   flag = 0;
   res.render('search.hbs', {
@@ -111,40 +111,36 @@ app.get('/products/:page', (req, res) => {
   var curUrl = req.params.page;
   var docRef = firebase.firestore().collection("Products").doc(curUrl);
   docRef.get().then(function(doc) {
-    let phone = doc.data().Phone;
-    if(flag === 0)
-    {
-          res.render('baseProduct.hbs', {
-            name: doc.data().Name,
-            price: doc.data().Price,
-            img: doc.data().Img,
-            location: doc.data().Location,
-            condition: doc.data().Condition,
-            phone: JSON.stringify(decrypt(phone)).substring(1, 11),
-            human: 'No'
-          });
-
+    let phone = JSON.stringify(decrypt(doc.data().Phone)).split("\\u")[0].substr(1);
+    if(flag === 0) {
+      res.render('baseProduct.hbs', {
+        name: doc.data().Name,
+        price: doc.data().Price,
+        img: doc.data().Img,
+        location: doc.data().Location,
+        condition: doc.data().Condition,
+        phone: phone,
+        human: 'No'
+      });
+    } else {
+      res.render('baseProduct.hbs', {
+        name: doc.data().Name,
+        price: doc.data().Price,
+        img: doc.data().Img,
+        location: doc.data().Location,
+        condition: doc.data().Condition,
+        phone: phone,
+        human: 'Yes'
+      });
     }
-    else
-    {
-          res.render('baseProduct.hbs', {
-            name: doc.data().Name,
-            price: doc.data().Price,
-            img: doc.data().Img,
-            location: doc.data().Location,
-            condition: doc.data().Condition,
-            phone: JSON.stringify(decrypt(phone)).substring(1, 11),
-            human: 'Yes'
-          });
-
-    }
-});
+  });
 });
 
 /////////////
 //functions//
 /////////////
 
+//for encrypting phone numbers before posting to firebase
 function encrypt(phone) {
     let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
     let encrypted = cipher.update(phone);
@@ -155,13 +151,14 @@ function encrypt(phone) {
     };
 }
 
+//for decrypting encrypted phone numbers from firebase
 function decrypt(phone) {
-    let iv = Buffer.from(phone.iv, 'hex');
-    let encryptedText = Buffer.from(phone.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-    decipher.setAutoPadding(false);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
+  let iv = Buffer.from(phone.iv, 'hex');
+  let encryptedText = Buffer.from(phone.encryptedData, 'hex');
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+  decipher.setAutoPadding(false);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
 }
 
@@ -180,95 +177,134 @@ var db = firebase.firestore();
 //  console.log(db);
 
 const request = require('request');
-function addData(name, price, condition, location, image, phone, category, email)
-{
+function addData(name, price, condition, location, image, phone, category, email) {
   console.log(image);
+  var thename = name;
+  var theprice = price;
+  var thecondition = condition;
+  var thelocation = location;
+  var theImg = image;
+  var theNumber = phone;
+  var theCategory = category
+  var encryptedphone = encrypt(phone);
+  var decryptedphone = decrypt(encryptedphone);
 
-    var thename = name;
-    var theprice = price;
-    var thecondition = condition;
-    var thelocation = location;
-    var theImg = image;
-    var theNumber = phone;
-    var theCategory = category
-    var encryptedphone = encrypt(phone);
-    var decryptedphone = decrypt(encryptedphone);
-
-    function getImageForPath(p){
-        //console.log(p);
-        global.XMLHttpRequest = require('xhr2');
-
-
-        var storageRef = firebase.storage().ref();
-        var spaceRef = storageRef.child(p);
-
-        storageRef.child(p).getDownloadURL().then(function(url) {
-            //console.log(db.collection("Products"));
-            //var fullurl = url;
-            console.log('hello');
-            db.collection("Products").add({
-                    Price: price,
-                    Name: name,
-                    Location: location,
-                    Condition: condition,
-                    Phone: encryptedphone,
-                    Img: url,
-                    Category: category,
-                    Email: email
-                  }).then(function(docRef) {
-                    console.log("Document written with ID: ", docRef.id);
-                    console.log(encryptedphone);
-                    console.log(decryptedphone);
-//update the products view
-                    //getProducts();
-                }).catch(function(error) {
-                    console.error("Error adding document: ", error);
-                });
-        }).catch(function(error) {
-            //catch error here
-            console.log(error);
-        });
-    }
-    getImageForPath('images/'+image);
+  function getImageForPath(p){
+    //console.log(p);
+    global.XMLHttpRequest = require('xhr2');
+    var storageRef = firebase.storage().ref();
+    var spaceRef = storageRef.child(p);
+    storageRef.child(p).getDownloadURL().then(function(url) {
+      //console.log(db.collection("Products"));
+      //var fullurl = url;
+      console.log('hello');
+      db.collection("Products").add({
+        Price: price,
+        Name: name,
+        Location: location,
+        Condition: condition,
+        Phone: encryptedphone,
+        Img: url,
+        Category: category,
+        Email: email
+      }).then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        console.log(encryptedphone);
+        console.log(decryptedphone);
+        //update the products view
+        //getProducts();
+      }).catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+    }).catch(function(error) {
+      //catch error here
+      console.log(error);
+    });
+  }
+  getImageForPath('images/'+image);
 }
 
-app.post('/firebase', function(req, res)
-{
-    var name=req.body.name;
-    var price=req.body.price;
-    var condition=req.body.condition;
-    var location=req.body.location;
-    var img = req.body.something;
-    var phone = req.body.phone_number;
-    var email = req.body.email;
-    var category=req.body.category;
-    console.log(email);
-    console.log(img);
+//post for adding content to firebase
+app.post('/firebase', function(req, res) {
+  var name=req.body.name;
+  var price=req.body.price;
+  var condition=req.body.condition;
+  var location=req.body.location;
+  var img = req.body.something;
+  var phone = req.body.phone_number;
+  var email = req.body.email;
+  var category=req.body.category;
+  console.log(email);
+  console.log(img);
 
-    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
- {
-   //return res.json({"responseError" : "Please select captcha first"});
- }
- const secretKey = "6Lcdi6IUAAAAAL-HspS-Y9UmWuoE0ToxT8BSXjnc";
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+  //return res.json({"responseError" : "Please select captcha first"});
+  }
+  const secretKey = "6Lcdi6IUAAAAAL-HspS-Y9UmWuoE0ToxT8BSXjnc";
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+    if(body.success !== undefined && !body.success) {
+      res.render('add.hbs', {
+        error:"Please complete the reCAPTCHA"
+      }); //return res.json({"responseError" : "Failed captcha verification"});
+    }
+    //res.json({"responseSuccess" : "Sucess"});
+    else {
+      addData(name, price, condition, location, img, phone, category, email);
+      res.redirect('/');
+    }
+  });
+});
 
- const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  //captcha lock for contact info in product details
+  app.post('/getcontact', (req, res) => {
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    //return res.json({"responseError" : "Please select captcha first"});
+  }
+  const secretKey = "6Lcdi6IUAAAAAL-HspS-Y9UmWuoE0ToxT8BSXjnc";
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+    if(body.success !== undefined && !body.success) {
+      console.log('Failed');    //return res.json({"responseError" : "Failed captcha verification"});
+    }
+    //res.json({"responseSuccess" : "Sucess"});
+    else {
+     flag = 1;
+     res.redirect('back');
+    }
+  });
+});
 
- request(verificationURL,function(error,response,body) {
-   body = JSON.parse(body);
-
-   if(body.success !== undefined && !body.success) {
-     res.render('add.hbs', {
-       error:"Please complete the reCAPTCHA"
-     });     //return res.json({"responseError" : "Failed captcha verification"});
-   }
-   //res.json({"responseSuccess" : "Sucess"});
-   else
-     {
-       addData(name, price, condition, location, img, phone, category, email);
-       res.redirect('/');
-     }
-
- });
+//post for search
+app.post('/search', function(req, res) {
+  var proname = req.body.productname;
+  var category=req.body.category;
+  var arr = [];
+  var curUrl = req.params.page;
+  var docRef = firebase.firestore().collection("Products").doc()
+  firebase.firestore().collection("Products").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      if((proname === doc.data().Name && category === doc.data().Category) || (proname === doc.data().Name && category === 'choose') || proname === '' && category === doc. data(). Category) {
+        //var tablename=Math.random();
+        var tablename='hello';
+        console.log('hello');
+        var name=doc.data().Name;
+        //var price=req.body.price;
+        var condition=doc.data().Condition;
+        var location= doc.data().Location;
+        var img =  doc.data().Img;
+        var price = doc.data().Price;
+        var id = doc._key.path.segments[6];
+        arr.push({Price: price, Name: name, Condition: condition, Location: location, Img: img, id: id});
+      }
+    });
+    console.log(arr);
+    res.render('results.hbs', {
+      arr: arr
+    });
+  });
 });
 
 //////////////////
@@ -293,69 +329,6 @@ app.post('/newUser', (request, response) => {
             var errorMessage = error.message;
         });
     }
-});
-
-app.post('/getcontact', (req, res) => {
-  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
-{
- //return res.json({"responseError" : "Please select captcha first"});
-}
-const secretKey = "6Lcdi6IUAAAAAL-HspS-Y9UmWuoE0ToxT8BSXjnc";
-
-const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-
-request(verificationURL,function(error,response,body) {
- body = JSON.parse(body);
-
- if(body.success !== undefined && !body.success) {
-   console.log('Failed');    //return res.json({"responseError" : "Failed captcha verification"});
- }
- //res.json({"responseSuccess" : "Sucess"});
- else
-   {
-     flag = 1;
-     res.redirect('back');
-
-   }
-});
-});
-
-app.post('/search', function(req, res)
-{
-    var proname = req.body.productname;
-    var category=req.body.category;
-    var arr = [];
-    var curUrl = req.params.page;
-    var docRef = firebase.firestore().collection("Products").doc()
-
-    firebase.firestore().collection("Products").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-
-          if((proname === doc.data().Name && category === doc.data().Category) || (proname === doc.data().Name && category === 'choose') || proname === '' && category === doc. data(). Category)
-          {
-            //var tablename=Math.random();
-            var tablename='hello';
-            console.log('hello');
-            var name=doc.data().Name;
-            //var price=req.body.price;
-            var condition=doc.data().Condition;
-            var location= doc.data().Location;
-            var img =  doc.data().Img;
-            var price = doc.data().Price;
-            var id = doc._key.path.segments[6];
-            arr.push({Price: price, Name: name, Condition: condition, Location: location, Img: img, id: id});
-
-
-        }
-  });
-  console.log(arr);
-
-  res.render('results.hbs', {
-    arr: arr
-  });
-
-});
-
 });
 
 //post for login and helper for username
