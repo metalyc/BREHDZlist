5ab8193f37c8f70547dba6995b9a2ffdaf7ec410
Evan Aprhys <evan.aprhys@gmail.com>
2019-05-17 12:51:03 -0700

    changed port to 8080

--- server.js
+++ server.js
@@ -473,7 +473,7 @@ app.get('*', (req, res) => {
 
 //start server
 app.use(express.static(__dirname));
-var server = app.listen(process.env.PORT || 8000, () => {
+var server = app.listen(process.env.PORT || 8080, () => {
     console.log('server is listening on port', server.address().port);
 });
 
